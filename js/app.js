da892cf97f2a81e0977123d47df031fe6752a24f
tkminhhuantk <33410153+tkminhhuantk@users.noreply.github.com>
2019-12-04 17:11:11 +0700

    V.7

--- js/app.js
+++ js/app.js
@@ -24,5 +24,23 @@ $(document).ready(function() {
 		      }
 		    }
 		});
-	})
+	}),
+	jQuery(function($){
+		$('.post-related').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:2
+		        },
+		        1000:{
+		            items:3
+		        }
+		    }
+		});
+	});
 })
